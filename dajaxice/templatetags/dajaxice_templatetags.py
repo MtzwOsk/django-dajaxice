7ac1762a5387d664b95b4245e4bf7611984a5d63
MtzwOsk <mtzw07@gmail.com>
2018-06-04 10:13:36 +0200

    Update dajaxice_templatetags.py

--- dajaxice/templatetags/dajaxice_templatetags.py
+++ dajaxice/templatetags/dajaxice_templatetags.py
@@ -3,8 +3,10 @@ import logging
 from django import template
 from django.middleware.csrf import get_token
 from django.conf import settings
-from django.core.files.storage import get_storage_class
-from django.urls import reverse
+try:
+    from django.urls import reverse
+except ImportError:
+    from django.core.urlresolvers import reverse
 from django.utils.safestring import mark_safe
 from dajaxice.core import dajaxice_config
 
